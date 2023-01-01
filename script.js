37e8b4d00c3e248581804a3b8edf582ac1967840
justin wang <justin.wang1ab@gmail.com>
2022-12-31 22:08:20 -0500

    Create a one-round playable game
    Add a dictionary that holds the selections that a certain selection
    wins against
    Add a function to capitalize the first letter of a word and set the
    rest to lowercase
    Add a function that determines whether the player wins or the computer
    wins and returns a message

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+// Each key beats each of the values in the lists
+let wins_against_dict = {
+  "Scissors": ["Paper", "Lizard"],
+  "Paper": ["Spock", "Rock"],
+  "Rock": ["Scissors", "Lizard"],
+  "Lizard": ["Spock", "Paper"],
+  "Spock": ["Rock", "Scissors"],
+}
+
 function randomNumberGen(num){
+  /* 
+  takes in an integer as an argument and generates a random integer 
+  between 0 and one less than the argument
+  */
   return Math.floor(Math.random() * num);
 }
 
 function getComputerChoice() {
+  // randomly generates a choice out of the 5 options
   let num = randomNumberGen(5);
   let choice;
 
@@ -26,3 +40,26 @@ function getComputerChoice() {
 
   return choice;
 }
+
+function capitalize(word){
+  // Capitalizes the first letter and set the rest to lower case
+  return word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
+}
+
+
+function playround(playerSelection, computerSelection){
+  // Takes two selections and determines the winner
+  let player = capitalize(playerSelection);
+  let message;
+  
+  if (player == computerSelection){
+    message = "Tie!";
+  } else if ((wins_against_dict[player]).includes(computerSelection)){
+    message = "You Win! " + player + " beats " + computerSelection;
+  } else {
+    message = "You Lose! " + computerSelection + " beats " + player;
+  }
+
+  return message;
+}
+
